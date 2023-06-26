b9f3550436d91c5843d8ad7a2360c4064d2163d6
Pouya <pouyamohammadyirbu@gmail.com>
2023-06-27 01:44:09 +0330

    adding base of TM

--- FL - proj 2/code/turing_machine.py
+++ FL - proj 2/code/turing_machine.py
@@ -1,21 +1,41 @@
-# This is a Turing Machine to calculate below function
-# f(n) = (3n+1)!
-
+# this is a turing machine base class to move on a liner tape
 class TuringMachine:
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, states, initial_states, final_states, transitions) -> None:
+        self.states = states
+        self.initial_state = initial_states
+        self.final_state = final_states
+        self.transitions = transitions
     
     # given function
-    def function(n: int) -> bool:
-        pass
-    
-# Run app main
-if __name__ == "__main__":
-    tn = TuringMachine()
-    n = input('Give me the n required for function:')
-    try:
-        tn.function(int(n))
-    except:
-        print('Function failed to operate!')
-    
+    def run(self, data:str)->str:
+        curr_state = self.initial_state
+        if len(data) == 0:
+            print('null input')
+            return ''
+        tape = '_' + data + '_'
+        print("data in: {} and taped to: ".format(data), end= '')
+        print(tape)
+        position = 0
+        while curr_state != self.final_state:
+            try:
+                print('flags: cs: {}, pos: {}, tape: {}'.format(curr_state, position, tape), end=' -- ')
+                curr_state, movement, write_value = self.transitions[curr_state][tape[position]]
+                print('ns: {}, move: {}, write_value: {}'.format(curr_state, movement, write_value))
+                # Write on given position 
+                tape = tape[:position] + write_value + tape[position+1:]
+                if movement == 'L' or movement == 'l':
+                    position -= 1
+                elif movement == 'R' or movement == 'r':
+                    position += 1       
+            except:
+                print('Turing Machine Failed!')
+                return ''
+        if tape[0] == '_':
+            tape = tape[1:]
+        if tape[len(tape)-1] == '_':
+            tape = tape[:len(tape)-1]
+        print('')
+        print('final tape is: ', end='')
+        print(tape)
+        return tape
